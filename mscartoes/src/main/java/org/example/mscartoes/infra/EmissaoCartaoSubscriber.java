f0884517e4fe87a8cec6318edde49591c41de715
Beatriz Gomes <beanascigom@gmail.com>
2025-03-23 11:40:55 -0300

    Endpoint para solicitação de emissão de cartão usando mensageria

--- mscartoes/src/main/java/org/example/mscartoes/infra/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/org/example/mscartoes/infra/EmissaoCartaoSubscriber.java
@@ -1,14 +1,41 @@
 package org.example.mscartoes.infra;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.example.mscartoes.domain.Cartao;
+import org.example.mscartoes.domain.ClienteCartao;
+import org.example.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
+import org.example.mscartoes.infra.repository.CartaoRepository;
+import org.example.mscartoes.infra.repository.ClienteCartaoRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmissaoCartaoSubscriber {
+
+    @Autowired
+    private final CartaoRepository cartaoRepository;
+    @Autowired
+    private final ClienteCartaoRepository clienteCartaoRepository;
 
     @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
     public void receberSolicitacaoEmissao(@Payload String payload) {
-        System.out.println(payload);
+        try {
+            var mapper = new ObjectMapper();
+            DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
+            Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+            ClienteCartao clienteCartao = new ClienteCartao();
+            clienteCartao.setCartao(cartao);
+            clienteCartao.setCpf(dados.getCpf());
+            clienteCartao.setLimite(dados.getLimiteLiberado());
+
+            clienteCartaoRepository.save(clienteCartao);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
